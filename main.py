2830d08a3941ba897c9e39669836da3c31f3a44d
kingdreamerr <jackmvula458@gmail.com>
2023-01-14 23:12:34 +0200

    Add function to checkk person with highest bid

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ def add_bid(name, price):
     oction.append({
         name:price
     })
+
+def winner(array):
+  highest = 0
+  winner = ""
+  for dic in array:
+    for person in dic:
+        if dic[person] > highest:
+            highest = dic[person]
+            winner = person
+  print(f"The winner is {winner} with a bid of ${highest}")
+
+more_bids = True
